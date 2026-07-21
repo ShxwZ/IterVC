f015c21b530ca67ca5d6c9ba2e7bc0fb34a49e0f
RausKZ <riusi0deagle@gmail.com>
2026-07-21 16:31:33 +0200

    (WIP)
    I added three temporary functions into the OscMediaService.cs. One does an approximation of the weight of each character depending on how they are displayed on VRChat. Another one takes the weight into account and marks wether the line has to be animated or not and the last one is the animation itself.

--- IterVC.Audio/OscMediaService.cs
+++ IterVC.Audio/OscMediaService.cs
@@ -1,12 +1,69 @@
 ﻿using BuildSoft.VRChat.Osc.Chatbox;
 using Microsoft.Extensions.Logging;
 using IterVC.Core.Interfaces;
+using Windows.UI.Text;
 
 namespace IterVC.Audio;
-
 public sealed class OscMediaService : IOscMediaService
 {
     private readonly ILogger<OscMediaService> _logger;
+    public double GetWeight(char c)
+    {
+        if ("WM@#%".Contains(c))
+        return 1.45;
+
+        if ("ABCDEFGHNOQRSUVXYZ".Contains(c))
+           return 1.0;
+
+        if ("abcdefghknopquvxyz".Contains(c))
+            return 0.8;
+
+        if ("iljrftI1".Contains(c))
+            return 0.5;
+
+        if (".,:;'|!".Contains(c))
+            return 0.4;
+
+        if (c == ' ')
+            return 0.45;
+
+        return 0.9;
+    }
+    private readonly double limit=24;
+    public string Animation(string line)
+    {
+        if (line.Length <= 1)
+            return line;
+        char[] result =line.ToCharArray();
+        char c = result[0];
+        for (int i = 0; i < result.Length - 1; i++)
+        result[i] = result[i + 1];
+        result[result.Length - 1] = c;
+        return new string(result);
+    }
+
+    public bool[] GetLinesAnimation(string message)
+    {
+        // supposing that all \r\n and \n are \v
+        string[] lines = message.Split('\v');
+        double Weight = 0;
+        bool[] animatedlines = new bool[lines.Length];
+        int counter = 0;
+        foreach(string line in lines)
+        {
+            Weight = 0;
+            foreach (char c in line)
+            {
+                Weight += GetWeight(c);
+            }
+            if(Weight > limit)
+            {
+                animatedlines[counter] = true;
+            }
+            counter++;
+        }
+        return animatedlines;
+    }
 
     public OscMediaService(ILogger<OscMediaService> logger) => _logger = logger;
 
