85d82ea164500c819d00593607af49ca7bdf5a50
Gabriel Pellizzón <39011276+ShxwZ@users.noreply.github.com>
2026-09-01 17:33:17 +0200

    Expose configurable microphone noise suppression

--- IterVC.Core/Interfaces/IMicrophoneService.cs
+++ IterVC.Core/Interfaces/IMicrophoneService.cs
@@ -16,6 +16,9 @@ public interface IMicrophoneService : IDisposable
 
     Task SetDeviceAsync(string microphoneDeviceId);
 
+    /// <summary>Enable or disable the real-time RNNoise stage.</summary>
+    void SetNoiseSuppressionEnabled(bool enabled);
+
     /// <summary>Datos PCM capturados en crudo, listos para ser inyectados en el mixer.</summary>
     event EventHandler<AudioDataEventArgs>? DataAvailable;
 }
